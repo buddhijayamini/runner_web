effb6762a0a89e10ff6356832b23580a59326a62
buddhijayamini <buddhigeethen@gmail.com>
2021-12-16 12:14:25 +0530

    auth changes

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -29,4 +29,47 @@ class APIController extends Controller
 
         return response()->json($o, $status);
     }
+
+    /**
+     * Returns an unauthorized (401) JSON response.
+     *
+     * @param  array $errors
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function responseUnauthorized($errors = ['Unauthorized.'])
+    {
+        return response()->json([
+            'status' => 401,
+            'errors' => $errors,
+        ], 401);
+    }
+
+    /**
+     * Returns a unprocessable entity (422) JSON response.
+     *
+     * @param  array $errors
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function responseUnprocessable($errors)
+    {
+        return response()->json([
+            'status' => 422,
+            'errors' => $errors,
+        ], 422);
+    }
+
+    /**
+     * Returns a server error (500) JSON response.
+     *
+     * @param  array $errors
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function responseServerError($errors = ['Server error.'])
+    {
+        return response()->json([
+            'status' => 500,
+            'errors' => $errors
+        ], 500);
+    }
+
 }
